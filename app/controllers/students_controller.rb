a688126abce09be19ce1d3f16e8cc0a7ed201beb
Gabriel Afflitto <gafflitto92@gmail.com>
2017-12-13 13:31:43 -0700

    Added student show page

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,22 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    binding.pry
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name)
+  end
+
 end
